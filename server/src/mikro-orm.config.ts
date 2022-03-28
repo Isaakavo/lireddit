a0caccda849cc8afffc30e0507d83549845d9625
Isaakavo <isaakhaas96@gmail.com>
2022-03-27 23:32:10 -0600

    first commit

---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { __prod__ } from './constants';
+import { Post } from './entities/Posts';
+import { User } from './entities/User';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post, User],
+  dbName: 'lireddit',
+  type: 'postgresql',
+  debug: !__prod__,
+  password: 'Weisses2',
+  allowGlobalContext: true
+} as Parameters<typeof MikroORM.init>[0];
