09ff3b2c05f9c99185ab6a6bdf8c7f100f15cc6f
Isaakavo <isaakhaas96@gmail.com>
2022-04-09 20:19:04 -0500

    complete the auth flow to change password

--- web/src/pages/change-password/[token].tsx
+++ web/src/pages/change-password/[token].tsx
@@ -1,12 +1,15 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import { NextPage } from 'next';
+import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { InputField } from '../../components/InputField';
 import Wrapper from '../../components/Wrapper';
 import { useChangePasswordMutation } from '../../generated/graphql';
+import { createUrqlClient } from '../../utils/createUrqlClient';
 import { toErrorMap } from '../../utils/toErrorMap';
+import NextLink from 'next/link';
 
 export const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
   const router = useRouter();
@@ -43,7 +46,14 @@ export const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
               label='New Password'
               type='password'
             />
-            {tokenError ? <Box color='red'>{tokenError}</Box> : null}
+            {tokenError ? (
+              <Flex>
+                <Box mr={2} color='red'>{tokenError}</Box>
+                <NextLink href='/forgot-password'>
+                  <Link>click here to get a new one </Link>
+                </NextLink>
+              </Flex>
+            ) : null}
             <Button mt={4} type='submit' isLoading={isSubmitting} color='teal'>
               Change Password
             </Button>
@@ -60,4 +70,4 @@ ChangePassword.getInitialProps = ({ query }) => {
   };
 };
 
-export default ChangePassword;
+export default withUrqlClient(createUrqlClient)(ChangePassword);
