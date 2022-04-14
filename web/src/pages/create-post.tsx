22483b6fc4003312a9108f37ed522b3327eedb05
Isaakavo <isaakhaas96@gmail.com>
2022-04-13 23:16:30 -0500

    added create post

---
+++ web/src/pages/create-post.tsx
@@ -0,0 +1,51 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { InputField } from '../components/InputField';
+import { Layout } from '../components/Layout';
+import Wrapper from '../components/Wrapper';
+import { useCreatePostMutation, useMeQuery } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { useIsAuth } from '../utils/useIsAuth';
+
+const CreatePost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  useIsAuth()
+  const [, createPost] = useCreatePostMutation();
+
+  return (
+    <Layout variant='small'>
+      <Formik
+        initialValues={{ title: '', text: '' }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values });
+          if (!error) {
+            router.push('/');
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name='title' placeholder='Title' label='Title' />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name='text'
+                placeholder='text...'
+                label='Boy'
+              />
+            </Box>
+
+            <Button mt={4} type='submit' isLoading={isSubmitting} color='teal'>
+              Create Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
