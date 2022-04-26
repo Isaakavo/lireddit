96ea959593ca33252a09139cb112038c2f78f444
Isaakavo <isaakhaas96@gmail.com>
2022-04-26 10:05:49 -0500

    added show creator on the index page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -15,7 +15,7 @@ import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
-  const [variables, setVariables] = useState({ limit: 10, cursor: null as null | string });
+  const [variables, setVariables] = useState({ limit: 15, cursor: null as null | string });
   const [{ data, fetching }] = usePostsQuery({ variables });
 
   if (!fetching && !data) {
@@ -36,7 +36,8 @@ const Index = () => {
         <Stack spacing={8}>
           {data!.posts.posts.map((x) => (
             <Box key={x.id} p={5} shadow='md' borderWidth='1px'>
-              <Heading fontSize='xl'>{x.title}</Heading>
+              <Heading fontSize='xl'>{x.title}</Heading> 
+              <Text>Posted by {x.creator.username}</Text>
               <Text mt={4}>{x.textSnippet}</Text>
             </Box>
           ))}
